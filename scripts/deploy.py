aee09b55c29d3942a1d7c0c481def698aa5eef58
t0astt <t0astt93@gmail.com>
2023-03-07 21:59:23 -0600

    Add deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,1 +1,9 @@
-from brownie import accounts
+#!/usr/bin/python3
+
+# Metamutt represents the contract name
+from brownie import Metamutt, accounts
+
+
+def main():
+    account = accounts.load("testaccount")
+    return Metamutt.deploy({'from': account})
